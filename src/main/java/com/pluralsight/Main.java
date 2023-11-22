935dc0e37322125ec35585ede4bb2b5ab4bbb0be
Kerry Metayer <KMetayer@appdev.yearup.org>
2023-11-22 14:30:15 -0500

    Just testing

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,6 +3,7 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
         System.out.println("We are creating a project");
+        System.out.println("Testing");
     }
 
 }

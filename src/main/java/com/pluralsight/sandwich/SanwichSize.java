04581ebc4ebc5c225f14aacb2e22063409c0e540
Kerry Metayer <KMetayer@appdev.yearup.org>
2023-11-27 10:53:33 -0500

    added sizes and price for sandwich size

--- src/main/java/com/pluralsight/sandwich/SanwichSize.java
+++ src/main/java/com/pluralsight/sandwich/SanwichSize.java
@@ -1,4 +1,15 @@
 package com.pluralsight.sandwich;
 
 public enum SanwichSize {
+    FOUR(5.50), EIGHT(7.00), TWELVE(8.50);
+    private final double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    SanwichSize(double price) {
+        this.price = price;
+
+    }
 }
